95bdbabf4f7a897eeb732ff597e7575543ff52e5
ajsalkt3399 <ajsalkt.dev@gmail.com>
2024-11-05 17:54:08 +0530

    latest

--- src/components/BannerSection/BannerLeftSection.jsx
+++ src/components/BannerSection/BannerLeftSection.jsx
@@ -43,10 +43,10 @@ function BannerLeftSection() {
     };
 
     return (
-        <div className="text-white max-xl:max-w-[554px]">
-            <h2 className='text-[rgb(191,193,197,60%)] mulishsemibold text-[16px] leading-[20px] xl:text-left font-semibold mb-[15px]'>{strings.Banner.heading}</h2>
-            <h1 className='text-[#FFFFFF] text-[34px] leading-[44px] text-center lg:text-[42px] lg:leading-[53px] xl:text-left mulishbold antialiased'>{strings.Banner.subheading}</h1>
-            <div className="text-[rgb(255,255,255,75%)] mulishsemibold font-normal text-[18px] leading-[28px] text-left mb-[15px]">{strings.Banner.description}</div>
+        <div className="text-white max-xl:max-w-[554px] max-w-[518px]">
+            <h2 className='text-[rgb(191,193,197,60%)] mulishsemibold max-xl:text-center text-[16px] leading-[20px] xl:text-left font-semibold mb-[15px]'>{strings.Banner.heading}</h2>
+            <h1 className='text-[#FFFFFF] text-[34px] leading-[44px] max-xl:text-center lg:text-[42px] lg:leading-[53px] xl:text-left mulishbold mb-[15px] antialiased'>{strings.Banner.subheading}</h1>
+            <div className="text-[rgb(255,255,255,75%)] mulishsemibold max-xl:text-center  font-normal text-[18px] leading-[28px] text-left mb-[19px]">{strings.Banner.description}</div>
             <div className='flex gap-2 max-sm:flex-col'>
                 <input
                     placeholder="Email"
